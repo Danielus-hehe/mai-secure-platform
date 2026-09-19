385ad05e601b77a20a196360526617fe7f1ae941
Daniel-C-420 <aguy4life32@gmail.com>
2026-09-19 19:34:35 +0300

    Fix minor pentru crearea de documente interne

--- MAI.Tests/AuthorizationPolicyTests.cs
+++ MAI.Tests/AuthorizationPolicyTests.cs
@@ -112,6 +112,12 @@ public class AuthorizationPolicyTests
             // (Login, respectiv PasswordWrite) și acceptă doar un token de 256 biți.
             "AuthController.CheckInvitation",
             "AuthController.ConfirmInvitation",
+            // Resetarea parolei din linkul trimis de administrator: cine și-a
+            // uitat parola nu se poate autentifica. Aceleași limitări ca la
+            // invitație (Login, respectiv PasswordWrite), token de 256 biți,
+            // de unică folosință, valabil PASSWORD_RESET_TOKEN_MINUTES.
+            "AuthController.CheckPasswordReset",
+            "AuthController.ResetPasswordWithToken",
         };
 
         var anonime = Controllers()
